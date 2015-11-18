04626a0fa3564c47776a1b83f96aac78d5a38781
Marc Nazarian <marc.nazarian@persistent.com>
2015-11-18 15:15:25 +0100

    Add first (dummy) test. Dummy and failing

---
+++ DojoUnitTestsCSharp/OnlineShoppingTests/ShoppingBasketTests.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShoppingBasketTests.cs" company="Persistent Systems">
+//   Persistent Systems
+// </copyright>
+// <summary>
+//   The shopping basket tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OnlineShoppingTests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The shopping basket tests.
+    /// </summary>
+    [TestFixture]
+    public class ShoppingBasketTests
+    {
+        /// <summary>
+        /// The dummy test.
+        /// </summary>
+        [Test]
+        public void DummyTest()
+        {
+            Assert.That(5, Is.EqualTo(6), "I was expecting that 5 should be equal to 6 :)");
+        }
+    }
+}

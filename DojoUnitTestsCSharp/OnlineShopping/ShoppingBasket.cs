b8232f8b8e841647c9709a75c76b94a98f5ebedf
Marc Nazarian <marc.nazarian@persistent.com>
2015-11-18 14:32:39 +0100

    Initial commit. Added classes without any unit tests

---
+++ DojoUnitTestsCSharp/OnlineShopping/ShoppingBasket.cs
@@ -0,0 +1,108 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShoppingBasket.cs" company="Persistent Systems">
+//   Persistent Systems
+// </copyright>
+// <summary>
+//   Shopping basket
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ShoppingBasket
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Shopping basket
+    /// </summary>
+    public class ShoppingBasket
+    {
+        /// <summary>
+        /// The shipping charges.
+        /// </summary>
+        private const double ShippingCharges = 10;
+
+        /// <summary>
+        /// The items.
+        /// </summary>
+        private readonly List<ShoppingItem> items;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShoppingBasket"/> class.
+        /// </summary>
+        public ShoppingBasket()
+        {
+            this.items = new List<ShoppingItem>();    
+        }
+
+        /// <summary>
+        /// Add an item into the shopping basket
+        /// </summary>
+        /// <param name="shoppingItem">
+        /// The shopping item to add into basket
+        /// </param>
+        public void AddItem(ShoppingItem shoppingItem)
+        {
+            this.items.Add(shoppingItem);
+        }
+
+        /// <summary>
+        /// Returns the number of items into the shopping basket
+        /// </summary>
+        /// <returns>
+        /// The number of Items into the shopping basket
+        /// </returns>
+        public int GetNumberOfItems()
+        {
+            return this.items.Count;
+        }
+
+        /// <summary>
+        /// The is empty.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsEmpty()
+        {
+            return this.GetNumberOfItems() == 0;
+        }
+
+        /// <summary>
+        /// The get price.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double GetPrice()
+        {
+            double totalPrice = 0;
+
+            foreach (ShoppingItem item in this.items)
+            {
+                totalPrice += item.Price;
+            }
+
+            return totalPrice + ShippingCharges;
+        }
+
+        /// <summary>
+        /// The get content description.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string GetContentDescription()
+        {
+            string contentDescription = string.Empty;
+
+            foreach (ShoppingItem item in this.items)
+            {
+                string itemDescription = string.Format("{0}: ${1}", item.Name, item.Price);
+
+                contentDescription += itemDescription;
+            }
+
+            return contentDescription;
+        }
+    }
+}
---
+++ DojoUnitTestsCSharp/OnlineShopping/ShoppingItem.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShoppingItem.cs" company="Persistent Systems">
+//   Persistent Systems
+// </copyright>
+// <summary>
+//   Shopping item
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ShoppingBasket
+{
+    /// <summary>
+    /// Shopping item
+    /// </summary>
+    public class ShoppingItem
+    {
+        /// <summary>
+        /// Gets or sets the item id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        public double Price { get; set; }
+    }
+}

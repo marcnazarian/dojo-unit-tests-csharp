3a2ddfa520816a8f27b724a0cee29b95fc112f17
Marc Nazarian <marc.nazarian@persistent.com>
2015-11-18 15:46:19 +0100

    Add 2 tests: get price when there is 1 item, and get price when several items

--- DojoUnitTestsCSharp/OnlineShoppingTests/ShoppingBasketTests.cs
+++ DojoUnitTestsCSharp/OnlineShoppingTests/ShoppingBasketTests.cs
@@ -98,5 +98,49 @@ namespace OnlineShoppingTests
             // assert
             Assert.That(this.shoppingBasket.GetNumberOfItems(), Is.EqualTo(3), "Basket should contain 3 items");
         }
+
+        /// <summary>
+        /// The can get the price of a single item.
+        /// </summary>
+        /// <param name="itemPrice">
+        /// The item Price.
+        /// </param>
+        /// <param name="expectedTotalBasketPrice">
+        /// The expected Total Basket Price.
+        /// </param>
+        [TestCase(1.23, 11.23)]
+        [TestCase(154.22, 164.22)]
+        [TestCase(9.99, 19.99)]
+        public void CanGetThePriceOfASingleItem(double itemPrice, double expectedTotalBasketPrice)
+        {
+            // arrange
+            ShoppingItem myItem = new ShoppingItem { Price = itemPrice };
+
+            // act
+            this.shoppingBasket.AddItem(myItem);
+
+            // assert
+            Assert.That(this.shoppingBasket.GetPrice(), Is.EqualTo(expectedTotalBasketPrice).Within(0.0001), "Total price of the basket should be " + expectedTotalBasketPrice);
+        }
+
+        /// <summary>
+        /// The can get the price of several items.
+        /// </summary>
+        [Test]
+        public void CanGetThePriceOfSeveralItems()
+        {
+            // arrange
+            ShoppingItem item1 = new ShoppingItem { Price = 5.21 };
+            ShoppingItem item2 = new ShoppingItem { Price = 0.18 };
+            ShoppingItem item3 = new ShoppingItem { Price = 145 };
+
+            // act
+            this.shoppingBasket.AddItem(item1);
+            this.shoppingBasket.AddItem(item2);
+            this.shoppingBasket.AddItem(item3);
+
+            // assert
+            Assert.That(this.shoppingBasket.GetPrice(), Is.EqualTo(160.39).Within(0.0001), "Total price of the basket should be 160.39 (150.39 + 10 for shipping)");
+        }
     }
 }
